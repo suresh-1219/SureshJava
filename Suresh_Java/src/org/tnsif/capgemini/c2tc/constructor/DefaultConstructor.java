43c677f8b12508f9f38a6fe3dffaf695bbf7100b
Suresh <sureshkosana333@gmail.com>
2024-12-17 23:09:58 +0530

    Update DefaultConstructor.java

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/constructor/DefaultConstructor.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/constructor/DefaultConstructor.java
@@ -1,9 +1,30 @@
 package org.tnsif.capgemini.c2tc.constructor;
 
-public class DefaultConstructor {
+class Myclass1
+{
+	String color;
+	int cost;
+	String brand;
+	
+	
+	//default constructor
+	Myclass1()
+	{
+		color="Pink";
+		cost=2000000;
+		brand="BMW";
+	}
+}
+
+
+
+public class DefaultConsturctor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Myclass1 obj=new Myclass1();
+		System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+		System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+		
 
 	}
 
