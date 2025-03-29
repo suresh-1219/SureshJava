b04bf24a710ec94f46b157327a5c25e86d5d31a7
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:20:27 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/jr_developer/KIA_Airbags.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/jr_developer/KIA_Airbags.java
@@ -1,10 +1,16 @@
 package org.tnsif.capgemini.c2tc.jr_developer;
 
 public class KIA_Airbags {
+	
+	public void disp_Airbags()
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	{
+		System.out.println("All KIA have airbags ");
 	}
-
+	 
+	public static void main(String[] args)
+	 {
+		 KIA_Speed_Limit obj=new KIA_Speed_Limit();
+		 obj.disp_Speed_limit();
+	 }
 }
