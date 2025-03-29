decb9f15b138ff08f269b61fc73aff5fcd964022
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:23:46 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/lambdaexpressiondemo/LambdaExpressionExample4.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/lambdaexpressiondemo/LambdaExpressionExample4.java
@@ -1,10 +1,15 @@
 package org.tnsif.capgemini.c2tc.lambdaexpressiondemo;
 
-public class LambdaExpressionExample4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+interface Addable{  
+    int add(int a,int b);  
+}  
+public class LambdaExpressionExample4{  
+    public static void main(String[] args) {     
+        // Multiple parameters in lambda expression  
+        Addable ad1=(a,b)->(a+b);  
+        System.out.println(ad1.add(10,20));     
+       // Multiple parameters with data type in lambda expression  
+        Addable ad2=(int a,int b)->(a+b);  
+        System.out.println(ad2.add(100,200));  
+    }  
+} 
