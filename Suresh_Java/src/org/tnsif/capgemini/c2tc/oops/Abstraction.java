5618d80b6467a488a268da8fcf09bdbb9fbe4e11
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:50:44 +0530

    commit 5

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/oops/Abstraction.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/oops/Abstraction.java
@@ -3,8 +3,50 @@ package org.tnsif.capgemini.c2tc.oops;
 public class Abstraction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MaheshDemo obj=new RameshDemo();
+		obj.calling();
+		obj.AIIntegration();
+		obj.SatelliteComm();
+		obj.HealthMontoring();
 	}
 
+}
+abstract class MaheshDemo{ //2015
+	
+
+	public void calling() //concrete method
+	{
+		System.out.println("calling");
+	}
+	
+	public abstract void AIIntegration(); //abstract method
+	public abstract void SatelliteComm();
+	public abstract void HealthMontoring();
+	
+}
+
+abstract class SureshDemo extends MaheshDemo //2016
+{
+	public void AIIntegration()
+	{
+		System.out.println("Phone is integrated with AIIntegration");
+	}
+	
+}
+
+class RameshDemo extends SureshDemo                 //2017
+{
+
+	@Override
+	public void SatelliteComm() {
+		System.out.println("Phone is integrated with SatelliteComm ");
+		
+	}
+
+	@Override
+	public void HealthMontoring() {
+		System.out.println("Phone is integrated with HealthMontoring");
+		
+	}
+	
 }
