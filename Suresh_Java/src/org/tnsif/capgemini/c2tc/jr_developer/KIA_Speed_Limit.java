512b6c228452a44ebdee34695d4ee50911819aa5
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:21:10 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/jr_developer/KIA_Speed_Limit.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/jr_developer/KIA_Speed_Limit.java
@@ -2,9 +2,9 @@ package org.tnsif.capgemini.c2tc.jr_developer;
 
 public class KIA_Speed_Limit {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 public void disp_Speed_limit()
+	{
+		System.out.println("All KIA have speedlimit ");
 	}
 
 }
