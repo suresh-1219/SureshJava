d264da493e1ffe9f71643d3784b1d1ed2b2e08ac
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:37:10 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/staticdemo/StaticVariable.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/staticdemo/StaticVariable.java
@@ -2,9 +2,34 @@ package org.tnsif.capgemini.c2tc.staticdemo;
 
 public class StaticVariable {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	String name;
+	double price;
+	public static String category="smart phone";
+	
+	public StaticVariable(String name,double price)
+	{
+		this.name=name;
+		this.price=price;
+	}
+	public void displayDetails()
+	{
+		System.out.println("Name " +name);
+		System.out.println("Price "+price);
+		System.out.println("category "+category);
+		System.out.println();
+		
+	}
+	
 
+	public static void main(String[] args) {
+		StaticVariable mobile1=new StaticVariable("Redmi",15000);
+		StaticVariable mobile2=new StaticVariable("Oppo",16000);
+
+		System.out.println("Mobile 1 information");
+		mobile1.displayDetails();
+		System.out.println("Mobile 2 information");
+		mobile2.displayDetails();
 	}
 
 }
+
