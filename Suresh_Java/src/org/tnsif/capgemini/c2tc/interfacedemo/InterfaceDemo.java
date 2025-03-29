84f126db32622aada84759bc57a7dc016aeee682
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:12:42 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/interfacedemo/InterfaceDemo.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/interfacedemo/InterfaceDemo.java
@@ -3,8 +3,17 @@ package org.tnsif.capgemini.c2tc.interfacedemo;
 public class InterfaceDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//you can't instantiate the interface itself
+		Vehicle car=new Car("Toyota corolla");
+		Vehicle bike=new Bike("Mountain Bike");
+		
+		car.start();
+		car.stop();
+		
+		bike.start();
+		bike.stop();
+		
+		System.out.println("Maximun speed for vehicle" +Vehicle.Max_Speed);
 	}
 
 }
