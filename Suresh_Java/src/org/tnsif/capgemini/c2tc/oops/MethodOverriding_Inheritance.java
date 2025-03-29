21a0810969f3f85217b88fff47660685bdeee328
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:57:06 +0530

    commit 5

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/oops/MethodOverriding_Inheritance.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/oops/MethodOverriding_Inheritance.java
@@ -1,10 +1,41 @@
 package org.tnsif.capgemini.c2tc.oops;
 
+class Animal4
+{
+	void makeSound()
+	{
+		System.out.println("Animal makes a sound");
+	}
+}
+class Dog4 extends Animal4
+
+{
+	void makeSound()
+	{
+		System.out.println("Dog is eating");
+	}
+}
+
+class Cat4 extends Animal4
+{
+	void makeSound()
+	{
+		System.out.println("Cat is eating");
+	}
+}
+
+
 public class MethodOverriding_Inheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Animal4 obj=new Animal4();
+		Dog4 obj1=new Dog4();
+		Cat4 obj2=new Cat4();
+		
+		obj.makeSound();
+		obj1.makeSound();
+		obj2.makeSound();
+		
 	}
 
 }
