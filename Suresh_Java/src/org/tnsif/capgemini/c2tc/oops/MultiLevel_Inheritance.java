e0b65f071d8a88c305a44070f3aad7293e3f9385
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:57:50 +0530

    commit 5

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/oops/MultiLevel_Inheritance.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/oops/MultiLevel_Inheritance.java
@@ -1,10 +1,36 @@
 package org.tnsif.capgemini.c2tc.oops;
 
+class Animal1
+{
+	void eat()
+	{
+		System.out.println("Animal is eating");
+	}
+}
+class Mammal extends Animal1
+{
+	void sleep()
+	{
+		System.out.println("Mammal is sleeping");
+		
+	}
+}
+class Dog2 extends Mammal 
+{
+	void bark()
+	{
+		System.out.println("Dog is barking");
+	}
+}
+
+
 public class MultiLevel_Inheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Dog2 dog=new Dog2();
+		dog.eat();
+		dog.sleep();
+		dog.bark();
 	}
 
 }
