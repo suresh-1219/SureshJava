69986e10ff068d6a7507715ad85b1b3c538a6ac7
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:34:12 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/sr_developer/KIA_Internal.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/sr_developer/KIA_Internal.java
@@ -1,9 +1,14 @@
 package org.tnsif.capgemini.c2tc.sr_developer;
-
+import org.tnsif.capgemini.c2tc.jr_developer.*;
 public class KIA_Internal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		KIA_Airbags obj=new KIA_Airbags();
+		obj.disp_Airbags();
+		
+//		 KIA_Internal obj1=new  KIA_Internal();
+//		obj1.disp_Speed_limit();
 
 	}
 
