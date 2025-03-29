b1cb3182077a00b848e5951ec9d4bd8538f56cdf
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:35:27 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/staticdemo/StaticBlock.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/staticdemo/StaticBlock.java
@@ -2,9 +2,26 @@ package org.tnsif.capgemini.c2tc.staticdemo;
 
 public class StaticBlock {
 
+	public void display()
+	{
+		System.out.println("instance block -1");
+	}
+	static
+	{
+		System.out.println("static block -1");
+	}
+	
+	static
+	{
+		System.out.println("static block -2");
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		StaticBlock obj=new StaticBlock ();
+		obj.display();
 
 	}
 
 }
+
