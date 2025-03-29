9ccc477078031da1528947f8956a92f69f51382a
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:12:02 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/interfacedemo/FunctionalInterfaceDemo.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/interfacedemo/FunctionalInterfaceDemo.java
@@ -1,9 +1,21 @@
 package org.tnsif.capgemini.c2tc.interfacedemo;
 
+class GreetingImp implements FunctionalInterface1
+{
+
+	@Override
+	public void sayHello(String name) {
+		System.out.println("Hello " +name+ " !");
+		
+	}
+	
+}
+
 public class FunctionalInterfaceDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FunctionalInterface1 obj=new GreetingImp();
+		obj.sayHello("Leela");
 
 	}
 
