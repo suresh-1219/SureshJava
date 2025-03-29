c335cec36eaf7021496449ea860ec06a67b6db9b
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:26:16 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/lambdaexpressiondemo/WithLambdaExpressionDemo.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/lambdaexpressiondemo/WithLambdaExpressionDemo.java
@@ -1,10 +1,16 @@
 package org.tnsif.capgemini.c2tc.lambdaexpressiondemo;
-
+@FunctionalInterface
+interface Drawable1
+{
+	public void draw();
+}
 public class WithLambdaExpressionDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int width=10;
+		Drawable1 d2=()->{
+			System.out.println("Drawing " + width);
+			};
+		d2.draw();
 	}
-
 }
