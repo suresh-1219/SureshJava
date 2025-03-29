8ee8d2d765f5b7be5ab0a96f158ac1bc88aa0307
Suresh <sureshkosana333@gmail.com>
2025-03-29 11:58:17 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/exception/NullPointerException.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/exception/NullPointerException.java
@@ -1,9 +1,18 @@
 package org.tnsif.capgemini.c2tc.exception;
 
-public class NullPointerException {
+public class NullPointerException1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str=null;
+		
+		try
+		{
+			System.out.println(str.length());
+		}catch(NullPointerException e)
+		{
+		System.out.println(e);
+		}
+		System.out.println("welcome");
 
 	}
 
