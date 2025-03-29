d767555aa5e84a294512829641daba5d5195fb73
Suresh <sureshkosana333@gmail.com>
2025-03-29 11:26:56 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/exception/ArithematicException.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/exception/ArithematicException.java
@@ -3,7 +3,20 @@ package org.tnsif.capgemini.c2tc.exception;
 public class ArithematicException {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number_one=50/5;
+		System.out.println(number_one); 
+		int number_two=50/10;
+		System.out.println(number_two);
+		try {
+			int number_three=50/0;
+			System.out.println(number_three); 
+		}catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		
+		int number_four=50/25;
+		System.out.println(number_four);
 
 	}
 
