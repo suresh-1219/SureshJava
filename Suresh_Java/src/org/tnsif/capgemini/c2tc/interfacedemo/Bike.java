c77ec8b504b55cab03c82f33dbc3af018f69d60b
Suresh <sureshkosana333@gmail.com>
2025-03-29 12:09:01 +0530

    commit 4

--- Suresh_Java/src/org/tnsif/capgemini/c2tc/interfacedemo/Bike.java
+++ Suresh_Java/src/org/tnsif/capgemini/c2tc/interfacedemo/Bike.java
@@ -1,10 +1,23 @@
 package org.tnsif.capgemini.c2tc.interfacedemo;
+//another class implementingn the same interface
+public class Bike implements Vehicle
+{
+     private String type;
+     
+     public Bike(String type)
+     {
+    	 this.type=type;
+     }
+	@Override
+	public void start() {
+		
+		System.out.println("Bike " + type + " is starting");
+	}
 
-public class Bike {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Override
+	public void stop() {
+		
+		System.out.println("Bike " + type+ " is stopping");
 	}
 
 }
